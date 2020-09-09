001cabf91627f2226e3e76469da15fb986ef052e
coryortega <coryortega3@gmail.com>
2020-09-08 20:37:04 -0700

    updated endpoints to use hosted DB

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,90 +1,77 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import DoggoCard from "./DoggoCard";
-import '../admin.css'
+import "../admin.css";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [doggos, setDoggos] = useState([])
+  const [doggos, setDoggos] = useState([]);
   const [query, setQuery] = useState("");
 
   useEffect(() => {
     const getDoggos = () => {
       axios
-        .get('http://localhost:4000/api/posts/images/')
-        .then(response => {console.log(response.data)
-          let data = response.data.filter( el => el.verified == null );
+        .get(process.env.REACT_APP_DOGGOS_KEY)
+        .then((response) => {
+          let data = response.data.filter((el) => el.verified == null);
           // const data = response.data
           setDoggos(data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getDoggos();
   }, [query]);
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
   const handleYes = (id) => {
-    console.log(doggos)
     axios
-    .put(`http://localhost:4000/api/posts/images/${id}`, { verified: true })
-    .then(response => {console.log(response.data)
-        console.log(doggos,id, id-1)
-        let doggosFiltered = doggos.filter( el => el.id !== id );
-        // doggos.splice(id-1, 1)
+      .put(`${process.env.REACT_APP_PUT_KEY}${id}`, { verified: true })
+      .then((response) => {
+        let doggosFiltered = doggos.filter((el) => el.id !== id);
         setDoggos([...doggosFiltered]);
-        
-    })
-    .catch(error => {
-      console.error('Server Error', error);
-    });
-  }
+      })
+      .catch((error) => {
+        console.error("Server Error", error);
+      });
+  };
 
   const handleNo = (id) => {
     axios
-    .put(`http://localhost:4000/api/posts/images/${id}`, { verified: false })
-    .then(response => {console.log(response.data)
-        console.log(doggos,id, id-1)
-        let doggosFiltered = doggos.filter( el => el.id !== id );
-        // doggos.splice(id-1, 1)
+      .put(`${process.env.REACT_APP_PUT_KEY}${id}`, { verified: false })
+      .then((response) => {
+        let doggosFiltered = doggos.filter((el) => el.id !== id);
         setDoggos([...doggosFiltered]);
-        
-    })
-    .catch(error => {
-      console.error('Server Error', error);
-    });
-    }
-
-  
+      })
+      .catch((error) => {
+        console.error("Server Error", error);
+      });
+  };
 
   return (
     <div>
-    <section className="character-list">
-      {doggos.map(doggo => (
-        <div className = "contain">
-        <DoggoCard
-            name={doggo.name}
-            image={doggo.img}
-            caption={doggo.caption}
-        />
-            <button 
-                onClick={() => handleNo(doggo.id)}
-                class="btn">NO
+      <section className="character-list">
+        {doggos.map((doggo) => (
+          <div className="contain">
+            <DoggoCard
+              name={doggo.name}
+              image={doggo.img}
+              caption={doggo.caption}
+            />
+            <button onClick={() => handleNo(doggo.id)} class="btn">
+              NO
             </button>
-            <button 
-                onClick={() => handleYes(doggo.id)}
-                class="btn1">YES
+            <button onClick={() => handleYes(doggo.id)} class="btn1">
+              YES
             </button>
-        </div>
-      ))}
-    </section>
+          </div>
+        ))}
+      </section>
     </div>
-    
   );
-  
 }
--- src/components/DoggoOTDCard.js
+++ src/components/DoggoOTDCard.js
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import { Tweet } from 'react-twitter-widgets'
-import './DoggoOTDCard.css'
+import axios from "axios";
+import { Tweet } from "react-twitter-widgets";
+import "./DoggoOTDCard.css";
 
+const DoggoOTDCard = (props) => {
+  const [doggoOTD, setDoggoOTD] = useState("1303407802275241990");
 
-const DoggoOTDCard = props => {
-
-    const [doggoOTD, setDoggoOTD] = useState("1303407802275241990")
-
-    // useEffect(() => {
-    //     const getDoggo = () => {
-    //       axios
-    //         .get('http://localhost:4000/api/posts/images/')
-    //         .then(response => {console.log(response.data)
-    //           setDoggoOTD(response.data);
-    //         })
-    //         .catch(error => {
-    //           console.error('Server Error', error);
-    //         });
-    //     }
-    //     getDoggo();
-    //   }, []);
+  // useEffect(() => {
+  //     const getDoggo = () => {
+  //       axios
+  //         .get('http://localhost:4000/api/posts/images/')
+  //         .then(response => {console.log(response.data)
+  //           setDoggoOTD(response.data);
+  //         })
+  //         .catch(error => {
+  //           console.error('Server Error', error);
+  //         });
+  //     }
+  //     getDoggo();
+  //   }, []);
 
   return (
     <div>
-        <h3>Doggo of the Day!</h3>
-        <div className="doggo">
-            <Tweet tweetId={doggoOTD} />
-        </div>
+      <h3>Doggo of the Day!</h3>
+      <div className="doggo">
+        <Tweet tweetId={doggoOTD} />
+      </div>
     </div>
   );
 };
