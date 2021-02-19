dc89340cf14bce0b68014f4556ed4c94e6e5fc85
Cory Ortega <coryortega@Corys-MacBook-Pro.local>
2021-02-19 15:44:10 -0800

    fixed analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Admin from './components/Admin/Admin';
 import Header from './components/Header/Header';
 import { Route } from "react-router-dom";
 import './App.css';
 import SubmitPage from './components/SubmitPage/SubmitPage';
+import ReactGA from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS)
+    ReactGA.pageview('/')
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <div className="App">
       <Header/>
